2e50db8fcba625b1b8b6f338fe12970afd6a737f
BuildTools <unconfigured@null.spigotmc.org>
2024-05-01 02:31:02 -0400

    fixed config not generating on servers

--- src/main/java/net/mcreator/midnightlurker/MidnightlurkerModClient.java
+++ src/main/java/net/mcreator/midnightlurker/MidnightlurkerModClient.java
@@ -6,13 +6,10 @@ import net.mcreator.midnightlurker.init.MidnightlurkerModEntityRenderers;
 import net.mcreator.midnightlurker.init.MidnightlurkerModModels;
 import net.mcreator.midnightlurker.init.MidnightlurkerModParticles;
 import net.mcreator.midnightlurker.network.MidnightlurkerModVariables;
-import net.mcreator.midnightlurker.procedures.LurkerconfigProcedure;
 
 public class MidnightlurkerModClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        LurkerconfigProcedure.execute();
-
         MidnightlurkerModVariables.initClient();
         MidnightlurkerHudRenders.init();
         MidnightlurkerModEntityRenderers.init();
